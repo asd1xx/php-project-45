64f732056376076abd508d4ef314c10dcbbb228a
asd2d <872122@mail.ru>
2024-07-02 15:46:25 +0300

    добавлен namespace

--- src/Cli.php
+++ src/Cli.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Brain\Games\Src;
 use function cli\line;
 use function cli\prompt;
 
@@ -8,5 +8,5 @@ function welcome()
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
-    line("Hello, {$name}");
+    line("Hello, {$name}!");
 }
