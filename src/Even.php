44860e4ac0c3269a03412089a2a758262be82349
asd2d <872122@mail.ru>
2024-07-04 23:03:30 +0300

    реализована логика для работы игры

--- src/Even.php
+++ src/Even.php
@@ -7,25 +7,33 @@ use function cli\prompt;
 
 function even()
 {
+    // Знакомство
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, {$name}!");
-    line('Answer "yes" if the number is even, otherwise answer "no".');
-    $rand = mt_rand(1, 15);
-    line("Question: {$rand}");
-    $answer = mb_strtolower(prompt('Your answer'));
-    $correct = line('Correct!');
-    $congratulations = line("Congratulations, {$name}!");
 
-    for ($i = 1; ($rand % 2 === 0 && $answer === 'yes' && $i < 3) || ($rand % 2 !== 0 && $answer === 'no' && $i < 3); $i++) {
-        $congratulations;
+    // Старт игры "Проверка на чётность"
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+
+    for ($i = 1; $i <= 3; $i++) {
         $rand = mt_rand(1, 15);
         line("Question: {$rand}");
         $answer = mb_strtolower(prompt('Your answer'));
-        if ($rand % 2 !== 0 && $answer === 'yes') {
-            line("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
-        } elseif ($rand % 2 === 0 && $answer === 'no') {
-            line("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, Bill!");
+        
+        if (($rand % 2 === 0 && $answer === 'yes') || ($rand % 2 !== 0 && $answer === 'no')) {
+            line('Correct!');
         }
+    
+        if ($rand % 2 !== 0 && $answer === 'yes') {
+            line("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {$name}!");
+            break;
+        } elseif ($rand % 2 === 0 && $answer === 'no') {
+            line("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {$name}!");
+            break;
+        }
+    }
+    
+    if ($i > 3) {
+        line("Congratulations, {$name}!");
     }
 }
