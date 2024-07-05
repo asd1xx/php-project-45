b8e933f66cd22e48e8ffe5dbff5fd6f810d50a18
asd2d <872122@mail.ru>
2024-07-05 23:36:35 +0300

    реализована общая логика приветствия

--- src/Engine.php
+++ src/Engine.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Project\Src;
+
+use function cli\line;
+use function cli\prompt;
+
+// Знакомство
+function userName()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
+    return $name;
+}
