9f7e1b07608e7301c0c73e247f724c7bc065bcb3
asd2d <872122@mail.ru>
2024-07-05 18:12:37 +0300

    реализована логика игры Калькулятор

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -7,5 +7,33 @@ use function cli\prompt;
 
 function calc()
 {
+    // Знакомство
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
 
+    // Старт игры "Калькулятор"
+    line('What is the result of the expression?');
+    $operators = ['+', '-', '*'];
+
+    for ($i = 1; $i <= 3; $i++) {
+        $operandOne = mt_rand(1, 15);
+        $operandTwo = mt_rand(1, 15);
+        $key = array_rand($operators);
+        $randOperator = $operators[$key];
+        $expression = "$operandOne $randOperator $operandTwo";
+        $resultExpression = (string)eval("return $expression;");
+        line("Question: {$operandOne} {$randOperator} {$operandTwo}");
+        $answer = mb_strtolower(prompt('Your answer'));
+        if ($resultExpression === $answer) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$resultExpression}'.\nLet's try again, {$name}!");
+            break;
+        }
+    }
+
+    if ($i > 3) {
+        line("Congratulations, {$name}!");
+    }
 }
