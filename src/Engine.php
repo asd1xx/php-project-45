d4b761f6d4923eaae801f1b625e83ad8c4daab81
asd2d <872122@mail.ru>
2024-07-08 13:25:07 +0300

    реализована общая логика игр в функции engine

--- src/Engine.php
+++ src/Engine.php
@@ -5,11 +5,26 @@ namespace Project\Src;
 use function cli\line;
 use function cli\prompt;
 
-// Знакомство
-function userName()
+function engine(string $rules, callable $getQuestion)
 {
     line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?');
-    line("Hello, {$name}!");
-    return $name;
+    $userName = prompt('May I have your name?');
+    line("Hello, {$userName}!");
+    line($rules);
+
+    for ($i = 0; $i < 3; $i++) {
+        [$question, $correctAnswer] = $getQuestion();
+        line("Question: {$question}");
+        $answer = mb_strtolower(prompt('Your answer'));
+        if ($correctAnswer === $answer) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.\nLet's try again, {$userName}!");
+            break;
+        }
+    }
+
+    if ($i === 3) {
+        line("Congratulations, {$userName}!");
+    }
 }
