9186a0e45e7c13910562ef320dcd3534266ca2cc
asd2d <872122@mail.ru>
2024-07-08 13:23:45 +0300

    вынесена логика игры в файл src/Engine.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -2,36 +2,22 @@
 
 namespace Project\Src\Games;
 
-use function cli\line;
-use function cli\prompt;
-use function Project\Src\userName;
+use function Project\Src\engine;
 
 function calc()
 {
-    $name = userName();
-
-    // Старт игры "Калькулятор"
-    line('What is the result of the expression?');
-    $operators = ['+', '-', '*'];
-
-    for ($i = 0; $i < 3; $i++) {
-        $operandOne = mt_rand(1, 15);
-        $operandTwo = mt_rand(1, 15);
+    $rules = 'What is the result of the expression?';
+    $getQuestion = function() {
+        $operators = ['+', '-', '*'];
+        $randOne = mt_rand(1, 15);
+        $randTwo = mt_rand(1, 15);
         $key = array_rand($operators);
         $randOperator = $operators[$key];
-        $expression = "$operandOne $randOperator $operandTwo";
-        $resultExpression = (string)eval("return $expression;");
-        line("Question: {$operandOne} {$randOperator} {$operandTwo}");
-        $answer = mb_strtolower(prompt('Your answer'));
-        if ($resultExpression === $answer) {
-            line('Correct!');
-        } else {
-            line("'{$answer}' is wrong answer ;(. Correct answer was '{$resultExpression}'.\nLet's try again, {$name}!");
-            break;
-        }
-    }
+        $question = "$randOne $randOperator $randTwo";
+        $correctAnswer = (string)eval("return $question;");
 
-    if ($i === 3) {
-        line("Congratulations, {$name}!");
-    }
+        return [$question, $correctAnswer];
+    };
+
+    engine($rules, $getQuestion);
 }
