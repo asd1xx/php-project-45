2ee937f0d7a92ea88bfd1efd27413de06aee9b39
asd2d <872122@mail.ru>
2024-07-16 23:41:25 +0300

    магические числа заменены на константы

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -4,6 +4,9 @@ namespace App\Games\Even;
 
 use function App\Engine\engine;
 
+const MIN = 1;
+const MAX = 15;
+
 /**
  * Игра "Проверка на чётность".
  * Суть игры в следующем: пользователю показывается случайное число от 1 до 15.
@@ -15,7 +18,7 @@ function even()
 {
     $rules = 'Answer "yes" if the number is even, otherwise answer "no".';
     $getQuestion = function () {
-        $question = mt_rand(1, 15);
+        $question = mt_rand(MIN, MAX);
         $correctAnswer = $question % 2 === 0 ? 'yes' : 'no';
 
         return [$question, $correctAnswer];
